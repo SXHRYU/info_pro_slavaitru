a7abe1d56c5cdb9c94fc0bde6503efab635d399a
Slava Maltsev <slavaxru@gmail.com>
2023-03-10 11:05:06 +0300

    update test data generator

--- test_data.py
+++ test_data.py
@@ -1,22 +1,24 @@
+import os
 import faker
 
 
-f = faker.Faker("ru_RU")
+def generate_fake_data(file_path: str | os.PathLike, /, count: int) -> None:
+    f = faker.Faker("ru_RU")
 
-with open("test.txt", "w", encoding="utf-8") as file:
-    for _ in range(100):
-        # fmt: off
-        file.write(
-            (
-                f.date() + "\t"
-                + f.bban() + "\t"
-                + f.text().replace("\n", " ") + "\t"
-                + f.pricetag() + "\t"
-                + f.email() + "\t"
-                + f.phone_number() + "\t"
-                + f.address() + "\t"
-                + f.time() + "\t"
-                + f.businesses_inn() + "\n"
+    with open(file_path, "w", encoding="utf-8") as file:
+        for _ in range(count):
+            # fmt: off
+            file.write(
+                (
+                    f.date() + "\t"
+                    + f.bban() + "\t"
+                    + f.text().replace("\n", " ") + "\t"
+                    + f.pricetag() + "\t"
+                    + f.email() + "\t"
+                    + f.phone_number() + "\t"
+                    + f.address() + "\t"
+                    + f.time() + "\t"
+                    + f.businesses_inn() + "\n"
+                )
             )
-        )
-        # fmt: on
+            # fmt: on
